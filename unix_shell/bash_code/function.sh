8598d010dd31bab2792297ffe7dad7eb85ac18fd
Mazaya Yumna <mazayayumnawijaya@gmail.com>
2026-01-19 09:39:07 +0700

    finalize function script

--- unix_shell/bash_code/function.sh
+++ unix_shell/bash_code/function.sh
@@ -7,3 +7,53 @@ function hello {
 hello
 hello
 hello
+
+#sum number of sequence
+function addseq {
+	#so that sum var accessible locally not globally
+	local sum=0
+	for elem in $@
+	do
+		let sum=sum+$elem
+	done
+	echo $sum
+}
+
+#multiply num of seq
+function multseq {
+	local kali=1
+	for elem in $@
+	do
+		let kali=kali*$elem
+	done
+	echo $kali
+}
+
+#is even
+function iseven {
+	res=$(($1 % 2))
+	if [ $res -eq 0 ]; then
+		echo "1"
+	else
+		echo "0"
+	fi
+}
+
+#n even
+function neven {
+	local sum=0
+	#quoted for safety
+	for elem in "$@"
+	do
+		num=$(iseven "$elem")
+		if [ $num -eq 1 ]; then
+			let sum=sum+1
+		fi
+	done
+	echo $sum
+}
+
+#how to run:
+#source function.sh
+#hello yumne
+#addseq 12 90 3
